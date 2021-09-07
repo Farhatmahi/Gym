17112af160e7f3378757b1f97deee6d527bcde59
Farhatmahi <farhatmahi007@gmail.com>
2021-09-07 17:37:44 +0600

    team

--- js/custom.js
+++ js/custom.js
@@ -13,4 +13,11 @@ $(function(){
         closeBackground : '#e23e38'
     })
 
+    //team slider
+    $('.team-slider').slick({
+        slidesToShow : 4,
+        slidesToScroll : 1,
+        arrows : false
+    })
+
 })
