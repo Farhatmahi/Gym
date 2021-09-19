3c996fa8f489289b7225dc4b611af2256b850572
Farhatmahi <farhatmahi007@gmail.com>
2021-09-19 19:19:22 +0600

    responsive xs

--- js/custom.js
+++ js/custom.js
@@ -107,21 +107,61 @@ $(function(){
         autoplay : true,
         arrows : true,
         slidesToShow : 5,
-        prevArrow : '<i class="slick-prev fas fa-chevron-left left-arrow"></i>',
-        nextArrow : '<i class="slick-next fas fa-chevron-right right-arrow"></i>',
+        prevArrow : '<button class="slick-prev"><i class="fas fa-chevron-left left-arrow"></i></button>',
+        nextArrow : '<button class="slick-next"><i class="fas fa-chevron-right right-arrow"></i></button>',
         centerMode : true,
         centerPadding : '0px',
         responsive: [
-            {
-              breakpoint: 992,
-              settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: false
-              }
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
             }
-          ]
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              arrows : false
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
     },500)
+
+    //back to top button
+    $(window).scroll(function(){
+      var scrolling = $(this).scrollTop();
+
+      if(scrolling > 1000){
+          $('.back-to-top').fadeIn()
+      }else{
+          $('.back-to-top').fadeOut()
+      }
+
+      if(scrolling > 400){
+          $('.navbar ul li.active').removeClass('active')
+      }else{
+          $('.navbar ul li:first-child').addClass('active')
+      }
+  })
+    $('.back-to-top').click(function(){
+      $('body, html').animate({
+        scrollTop : 0, 
+      },1500)
+    })
 
 })
